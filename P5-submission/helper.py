14a0a87f51099093b6b50acaee57f842ae2cc167
Shuang Xia <shuang.xia@mydrivesolutions.com>
2018-04-17 19:14:54 +0100

    completed model

--- P5-submission/helper.py
+++ P5-submission/helper.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.image as mpimg
 from skimage.feature import hog
+import random
 
 def convert_color(img, conv_from='RGB', conv_to='YCrCb'):
     if conv_from==conv_to:
@@ -15,26 +16,28 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                         vis=False, feature_vec=True):
     # Call with two outputs if vis==True
     if vis == True:
-        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                                  cells_per_block=(cell_per_block, cell_per_block), block_norm= 'L2-Hys',
-                                  transform_sqrt=True, 
+        features, hog_image = hog(img, orientations=orient, 
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), 
+                                  transform_sqrt=False, 
                                   visualise=vis, feature_vector=feature_vec)
         return features, hog_image
     # Otherwise call with one output
     else:      
-        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                       cells_per_block=(cell_per_block, cell_per_block), block_norm= 'L2-Hys',
-                       transform_sqrt=True, 
+        features = hog(img, orientations=orient, 
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), 
+                       transform_sqrt=False, 
                        visualise=vis, feature_vector=feature_vec)
         return features
 
 
 # Define a function to compute binned color features  
 def bin_spatial(img, size=(32, 32)):
-    # Use cv2.resize().ravel() to create the feature vector
-    spatial_features = cv2.resize(img, size).ravel() 
-    # Return the feature vector
-    return spatial_features
+    color1 = cv2.resize(img[:,:,0], size).ravel()
+    color2 = cv2.resize(img[:,:,1], size).ravel()
+    color3 = cv2.resize(img[:,:,2], size).ravel()
+    return np.hstack((color1, color2, color3))
 
 # Define a function to compute color histogram features 
 # NEED TO CHANGE bins_range if reading .png files with mpimg!
@@ -88,23 +91,70 @@ def single_img_features(image, color_space='RGB', spatial_size=(32, 32),
         file_features.append(hog_features)
     return np.concatenate(file_features)
 
-def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
+def extract_features(files, augment=False, color_space='RGB', spatial_size=(32, 32),
                         hist_bins=32, orient=9, 
                         pix_per_cell=8, cell_per_block=2, hog_channel=0,
                         spatial_feat=True, hist_feat=True, hog_feat=True):
     # Create a list to append feature vectors to
     features = []
     # Iterate through the list of images
-    for file in imgs:
+    for file in files:
         # Read in each one by one
         image = mpimg.imread(file)
-        file_features = single_img_features(image, color_space=color_space, 
+        file_features = single_img_features(image, color_space=color_space,
                         spatial_size=spatial_size, hist_bins=hist_bins, 
                         orient=orient, pix_per_cell=pix_per_cell, 
                         cell_per_block=cell_per_block, 
                         hog_channel=hog_channel, spatial_feat=spatial_feat, 
                         hist_feat=hist_feat, hog_feat=hog_feat)
         features.append(file_features)
+        if augment:
+            # Flip image
+            image_flip = cv2.flip(image, 1)
+            file_features = single_img_features(image, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+            features.append(file_features)
+            
+#             # Rotate image
+#             angle = random.randint(-25, 25)
+#             rotation_matrix = cv2.getRotationMatrix2D((32, 32), angle, 1)
+#             img_rotation = cv2.warpAffine(image, rotation_matrix, (64, 64))
+#             file_features = single_img_features(image, color_space=color_space,
+#                         spatial_size=spatial_size, hist_bins=hist_bins, 
+#                         orient=orient, pix_per_cell=pix_per_cell, 
+#                         cell_per_block=cell_per_block, 
+#                         hog_channel=hog_channel, spatial_feat=spatial_feat, 
+#                         hist_feat=hist_feat, hog_feat=hog_feat)
+#             features.append(file_features)
+            
+#             # Blur image
+#             blur_amount = random.randrange(1,10,2)
+#             blur_image = cv2.GaussianBlur(image,(blur_amount,blur_amount),0)
+#             file_features = single_img_features(image, color_space=color_space,
+#                         spatial_size=spatial_size, hist_bins=hist_bins, 
+#                         orient=orient, pix_per_cell=pix_per_cell, 
+#                         cell_per_block=cell_per_block, 
+#                         hog_channel=hog_channel, spatial_feat=spatial_feat, 
+#                         hist_feat=hist_feat, hog_feat=hog_feat)
+#             features.append(file_features)
+
+            # Crop and resize image
+            x = random.randint(0,32)
+            y = random.randint(0,32)
+            crop_img = image[x:x+32, y:y+32]
+            newimage = cv2.resize(crop_img,(64,64))
+            file_features = single_img_features(image, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+            features.append(file_features)
+
     # Return list of feature vectors
     return features
 
@@ -165,11 +215,11 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
 
 # Define a function you will pass an image 
 # and the list of windows to be searched (output of slide_windows())
-def search_windows(img, windows, clf, scaler, color_space='RGB', 
+def search_windows(img, windows, clf, scaler, color_space='RGB', hog_color_space='RGB', 
                     spatial_size=(32, 32), hist_bins=32, 
                     hist_range=(0, 256), orient=9, 
                     pix_per_cell=8, cell_per_block=2, 
-                    hog_channel=0, spatial_feat=True, 
+                    hog_channel=0, color_hist_channels=[1,2], spatial_feat=True, 
                     hist_feat=True, hog_feat=True):
 
     #1) Create an empty list to receive positive detection windows
